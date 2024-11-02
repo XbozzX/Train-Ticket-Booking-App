0f50bff883873e7d1de6d34ff48a6236841b3650
XbozzX <afham821@gmail.com>
2024-11-03 06:40:48 +0800

    Fix the CORS error -updated vercel.json -updated index.js

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,10 @@ app.use(
 //create middleware for parsing the page location to the http req
 app.use("/api/trains", trainAPI);
 mongoose
-  .connect(`${process.env.MONGODB_URI}`)
+  .connect(`${process.env.MONGODB_URI}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Connect");
 
